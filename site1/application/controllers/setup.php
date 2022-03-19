8707a9d364d75dca3eed68895bad386d37f767a4
Mark Ryk <marco.look@outlook.com>
2022-03-19 17:48:12 -0300

    criado banco de dados e iniciando criação do painel de setup do sistema

---
+++ site1/application/controllers/setup.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Setup extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->load->model('option_model', 'option');
+	}
+
+	public function index(){
+		if ($this->option->get_option('setup_executado') == 1):
+			redirect('setup/alterar', 'refresh');
+		else:
+			//não instalado, mostra tela de setup
+			redirect('setup/instalar', 'refresh');
+		endif;
+
+	}
+
+	public function instalar(){
+		if ($this->option->get_option('setup_executado') == 1):
+			//setup ok, mostrar tela para editar dados de setup
+			redirect('setup/alterar', 'refresh');
+		endif;
+
+		//carrega view
+		$dados['titulo'] = 'Setup do sistema - MarkRyk site';
+		$dados['h2'] = "Setup do sistema";
+		$this->load->view('painel/setup', $dados);
+	}
+
+}
